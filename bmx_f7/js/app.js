47a7cb235fc24a2105506319ecb1b0bfd1e06b36
Phillip Penny <phillippenny@gmail.com>
2017-12-10 22:30:17 +0000

    Creating Framework7 version of app WITHOUT Vue.js

---
+++ bmx_f7/js/app.js
@@ -0,0 +1,17 @@
+/**
+ * @Author: alcwynparker
+ * @Date:   2017-12-07T22:56:21+00:00
+ * @Last modified by:   alcwynparker
+ * @Last modified time: 2017-12-07T23:01:00+00:00
+ */
+ // Initialize app
+ var myApp = new Framework7();
+
+ // If we need to use custom DOM library, let's save it to $$ variable:
+ var $$ = Dom7;
+
+ // Add view
+ var mainView = myApp.addView('.view-main', {
+   // Because we want to use dynamic navbar, we need to enable it for this view:
+   dynamicNavbar: true
+ });
